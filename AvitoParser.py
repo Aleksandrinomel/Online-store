4c4f746bebd0534052173d362090fad5c574b2dd
Aleksandr <aleksandrinomel@mail.ru>
2018-09-27 20:57:35 +0400

    slightly modified

--- AvitoParser.py
+++ AvitoParser.py
@@ -1,22 +1,89 @@
 import re
-from bs4 import BeautifulSoup
 import requests
+from datetime import datetime, timedelta
+import time
 
-avito_html = requests.get('https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty')
-text = BeautifulSoup(avito_html.text, "html.parser")
-pages = text.select('.pagination-page')
-for page in pages:
-    if page.getText() == 'Последняя':
-        m = re.search('(p=)\w+', page.get('href'))
-        total_pages = int(m.group(0)[2:])
+from bs4 import BeautifulSoup
 
-links = []
-l = 0
-for number_page in range(1, total_pages + 1):
-    link = 'https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p=' + str(number_page)
-    avito_html = requests.get(link)
+
+def parser():
+    proxies = {
+
+        'https': 'https://90.173.50.18:8080'
+    }
+
+    avito_html = requests.get('https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty',
+                              proxies=proxies)
     text = BeautifulSoup(avito_html.text, "html.parser")
-    items = text.select('.item-description-title-link')
-    for i in items:
-        href = 'https://www.avito.ru' + i.get('href')
-        links.append(href)
+    pages = text.select('.pagination-page')
+
+    # Определяет кол-во страниц, на которых расположены нужные объявления
+    for page in pages:
+        if page.getText() == 'Последняя':
+            m = re.search('(p=)\w+', page.get('href'))
+            total_pages = int(m.group(0)[2:])
+
+    # Ходит по страницам, ищет ссылки на товар и записывает их в список links
+    links = []
+    for number_page in range(1, total_pages + 1):
+        link = 'https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty?p=' + str(number_page)
+        avito_html = requests.get(link, proxies=proxies)
+        text = BeautifulSoup(avito_html.text, "html.parser")
+        items = text.select('.item-description-title-link')
+        for i in items:
+            href = 'https://www.avito.ru' + i.get('href')
+            links.append(href)
+            print(href)
+        time.sleep(0.5)
+    print(links)
+
+    # Ходит по ссылкам на товар и заполняет список словарей
+    data_dicts = []
+    months = {'марта': '03', 'апреля': '04', 'мая': '05', 'июня': '06', 'июля': '07', 'августа': '08',
+              'сентября': '09', 'октября': '10', 'ноября': '11', 'декабря': '12', 'января': '01', 'февраля': '02'}
+
+    for item_link in links:
+        try:
+            item_html = requests.get(item_link, proxies=proxies)
+            item_text = BeautifulSoup(item_html.text, "html.parser")
+            info_dict = {}
+
+            meta_data_list = item_text.select('.title-info-metadata-item')[0].getText().split()
+            if meta_data_list[3] == "сегодня":
+                info_dict['avito_date_publication'] = datetime.now().strftime('%Y-%m-%d')
+            elif meta_data_list[3] == 'вчера':
+                yesterday = datetime.now() - timedelta(days=1)
+                info_dict['avito_date_publication'] = yesterday.strftime('%Y-%m-%d')
+            else:
+                info_dict['avito_date_publication'] = datetime.now().strftime('%Y') + '-' + months[
+                    meta_data_list[4]] + '-' + meta_data_list[3]
+
+            info_dict['avito_time_publication'] = meta_data_list[-1]
+
+            info_dict['id'] = meta_data_list[1].strip(',')
+
+            info_dict['name'] = item_text.select('.title-info-title-text')[0].getText()
+
+            info_dict['price'] = item_text.select('.js-item-price')[0].getText()
+
+            info_dict['adress'] = ''.join(item_text.select('.item-map-location')[0].getText().split()[1:-2])
+
+            info_dict['ad_text'] = item_text.select('.item-description-text')[0].getText()
+
+#            info_dict['phone'] = item_text.select('.item-popup-content')
+
+            image_tags = item_text.select('.gallery-img-frame')
+            image_links = ''
+            for image_tag in image_tags:
+                image_links += image_tag['data-url'] + ','
+            info_dict['photo_link'] = image_links
+            data_dicts.append(info_dict)
+            print(info_dict)
+            time.sleep(0.5)
+        except:
+            continue
+    return data_dicts
+
+
+if __name__ == "__main__":
+    parser()
