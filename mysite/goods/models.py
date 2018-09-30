48a8728caeb64143092744b82cfe5cf6549f6e57
sobrabd <42965564+sobrabd@users.noreply.github.com>
2018-09-30 16:18:14 +0300

    models uploaded

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+ from django.db import models
 
 # Create your models here.
+
+class Goods(models.Model):
+	name = models.CharField(max_length=100)
+	avito_ad_number = models.IntegerField(max_length=100)
+	publication_date = models.DateTimeField()
+	publication_time = models.TimeField()
+	photo_link = models.CharField(max_length=250)
+	adress = models.CharField(max_length=250)
+	price = models.CharField(max_length=250)
+	text = models.TextField()
