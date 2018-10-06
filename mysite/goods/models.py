3eedde80ff57f67bc69f37ddd5e992552391f688
Sobir Abdullaev <sobirabd@yandex.com>
2018-10-06 15:49:44 +0300

    new commit

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -1,11 +1,11 @@
- from django.db import models
+from django.db import models
 
 # Create your models here.
 
 class Goods(models.Model):
-	name = models.CharField(max_length=100)
-	avito_ad_number = models.IntegerField(max_length=100)
-	publication_date = models.DateTimeField()
+	name = models.CharField(max_length=250)
+	avito_ad_number = models.IntegerField()
+	publication_date = models.DateField()
 	publication_time = models.TimeField()
 	photo_link = models.CharField(max_length=250)
 	adress = models.CharField(max_length=250)
