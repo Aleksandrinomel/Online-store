fbc57f8f41b42cb4542762d6074ff4a8c9ca79d0
Sobir Abdullaev <sobirabd@yandex.com>
2018-10-06 20:52:13 +0300

    new commit it works

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Goods(models.Model):
-	name = models.CharField(max_length=250)
-	avito_ad_number = models.IntegerField()
-	publication_date = models.DateField()
-	publication_time = models.TimeField()
-	photo_link = models.CharField(max_length=250)
-	adress = models.CharField(max_length=250)
-	price = models.CharField(max_length=250)
-	text = models.TextField()
+	name = models.TextField(null=True)
+	avito_ad_number = models.IntegerField(null=True)
+	publication_date = models.DateField(null=True)
+	publication_time = models.TimeField(null=True)
+	photo_link = models.TextField(null=True)
+	adress = models.TextField(null=True)
+	price = models.TextField(null=True)
+	text = models.TextField(null=True)
