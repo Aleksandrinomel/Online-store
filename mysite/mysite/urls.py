84f1b5f4eeca977e6d061ae467cb53b3f1ecd812
Sobir <sobirabd@yandex.ru>
2018-10-12 23:40:27 +0300

    changed view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from goods import views
+
 urlpatterns = [
     path('goods/', include('goods.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
