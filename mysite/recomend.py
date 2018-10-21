d09d4ff8f8623245985baab9b1c7305b728af1b4
Sobir <sobirabd@yandex.ru>
2018-10-21 22:25:29 +0300

    poka ne rabotaet

--- mysite/recomend.py
+++ mysite/recomend.py
@@ -12,11 +12,12 @@ import numpy as np
 # сбор всех описаний наших объявлений
 
 all_goods = Goods.objects.all()
+
+
 dataset = []
 
-for i in range(1, len(all_goods) + 1):
+for good in all_goods:
     try:
-        good = Goods.objects.get(id=i)
         good_text = good.text
         good_text = re.sub("^\s+|\n|\r|\s+$", '', good_text)
 
@@ -32,19 +33,21 @@ for i in range(1, len(all_goods) + 1):
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix = tfidf_vectorizer.fit_transform(dataset)
 print(tfidf_matrix.shape)
-result = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
+result = cosine_similarity(tfidf_matrix, tfidf_matrix)
 print(result)
+print(result.shape)
 sorted_result = np.sort(result)
-indices = [-5, -4, -3, -2, -1]
+indices = [-6, -5, -4, -3, -2]
 nearest_five = np.take(sorted_result, indices)
 print(nearest_five.shape)
 
-nearest_five_list = list(nearest_five)
-print(nearest_five_list)
-result_list = list(result)
-index_list = []
-for ii in range(len(nearest_five_list) + 1):
-    [i for i,x in enumerate(result_list) if x==nearest_five_list[ii]]
-    index_list.append(i)
 
-print(index_list)
+max_index = np.argpartition(result, -7)[-2:-1]
+print(max_index)
+
+#for ii in range(len(nearest_five_list) + 1):
+#print(nearest_five_list[0])
+    #[i for i,x in enumerate(result_list) if x==nearest_five_list[ii]]
+    #index_list.append(i)
+
+#print(index_list)
