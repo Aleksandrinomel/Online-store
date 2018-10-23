fc6d30ebda38ca5c24f67502252b47e99d5b9ea7
Sobir <sobirabd@yandex.ru>
2018-10-23 12:37:33 +0300

    working recomendations

--- mysite/recomend.py
+++ mysite/recomend.py
@@ -29,16 +29,16 @@ for number, goods in enumerate(all_goods):
 # Векторизация текста с помощью sklearn
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix = tfidf_vectorizer.fit_transform(dataset)
-#print(tfidf_matrix.shape)
+
 result = cosine_similarity(tfidf_matrix, tfidf_matrix)
-#print(result)
+
 
 
 
 all_cosine = list(enumerate(result[number], 1))
-print(all_cosine)
+
 all_cosine_number_sorted = sorted(all_cosine,key=lambda all_cosine: all_cosine[1])
-print(all_cosine_number_sorted) 
+
 five_cosine_number_sorted = all_cosine_number_sorted[-6:-1]
 nearest_avito_ad_number = []
 for i in five_cosine_number_sorted:
@@ -50,12 +50,3 @@ print(nearest_avito_ad_number)
 
 
 
-#max_index = np.argpartition(result, -7)[-2:-1]
-#print(max_index)
-
-#for ii in range(len(nearest_five_list) + 1):
-#print(nearest_five_list[0])
-    #[i for i,x in enumerate(result_list) if x==nearest_five_list[ii]]
-    #index_list.append(i)
-
-#print(index_list)
