5001df76ac758c8552c68ed2defa07aa90d18cd7
Sobir Abdullaev <sobirabd@yandex.com>
2018-10-27 16:25:27 +0300

    new recomendation

--- mysite/recomend.py
+++ mysite/recomend.py
@@ -9,44 +9,50 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
+
+
 # сбор всех описаний наших объявлений
-
-all_goods = Goods.objects.all()
-avito_id = 1193530238
-
-dataset = []
-
-for good in all_goods:
-    good_text = good.text
-    good_text = re.sub("^\s+|\n|\r|\s+$", '', good_text)
-    dataset.append(good_text)
-
-for number, goods in enumerate(all_goods):
-	if goods.avito_ad_number == avito_id:
-		print(number)
+def get_recomendation(avito_id):
+    all_goods = Goods.objects.all()
 
 
-# Векторизация текста с помощью sklearn
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(dataset)
+    dataset = []
 
-result = cosine_similarity(tfidf_matrix, tfidf_matrix)
+    for good in all_goods:
+        try:
+            good_text = good.text
+            good_text = re.sub("^\s+|\n|\r|\s+$", '', good_text)
+            dataset.append(good_text)
+        except BaseException as e:
+            print(e)
+            continue
+    num = 0
+    for number, goods in enumerate(all_goods):
+        if goods.avito_ad_number == avito_id:
+            num = number
+            break
 
+            
 
+    #print(dataset)
+    # Векторизация текста с помощью sklearn
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(dataset)
 
-
-all_cosine = list(enumerate(result[number], 1))
-
-all_cosine_number_sorted = sorted(all_cosine,key=lambda all_cosine: all_cosine[1])
-
-five_cosine_number_sorted = all_cosine_number_sorted[-6:-1]
-nearest_avito_ad_number = []
-for i in five_cosine_number_sorted:
-	[nearest_avito_ad_number.append(value.avito_ad_number) for num, value in enumerate(all_goods) if num == i[0]]
-
-
-print(nearest_avito_ad_number)
+    result = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
 
 
+
+    all_cosine = list(enumerate(result[num], 1))
+
+    all_cosine_number_sorted = sorted(all_cosine,key=lambda all_cosine: all_cosine[1])
+
+    five_cosine_number_sorted = all_cosine_number_sorted[-6:-1]
+    nearest_avito_ad_number = []
+    for i in five_cosine_number_sorted:
+        [nearest_avito_ad_number.append(value.avito_ad_number) for num, value in enumerate(all_goods) if num == i[0]]
+
+    
+    return nearest_avito_ad_number
 
